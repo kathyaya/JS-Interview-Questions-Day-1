b9e5ef343cb75a8f80b2a35ea38f8bbbf31fdbb2
kathyayanikr <kathyayanikr2112001@gmail.com>
2023-01-04 01:47:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+// let a = 10;
+// let b= "10";
+// console.log(a == b);
+// console.log( a === b);
+
+// let p="EA10";
+// let q="10";
+// console.log(typeof(p+q));
+// console.log(typeof(p*q));
+
+// let a1=10;
+// let b2=20;
+// console.log(a1 == b2);
+// console.log(a1 === b2);
+
+// var a;
+// let b;
+
+// console.log(a);
+// console.log(b);
+
+// var q=10;
+// console.log(q);
+// q=20;
+// console.log(q);
+// var q=30;
+// console.log(q);
+
+// function abc(){
+//     var z = "EA18";
+//     console.log(z);
+// }
+// abc();
+// console.log(z);
+
+// {
+//     var M="India";
+//     let N="EA18";
+//     const O ="Prepbytes";
+//     console.log(M,N,O)
+// }
+// console.log(O);
+
+// console.log(p,h,k);
+// var p= "P";
+// let h ="EA18";
+// const k = "K";
+// Hois();
+// function Hois(){
+//     console.log("Hoisting");
+// }
